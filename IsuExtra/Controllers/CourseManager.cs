05778c5232852a8f9c232d4da608c51684bafaf0
lifeonh1gh <lifeonh1gh@mail.ru>
2021-10-21 16:14:38 +0300

    Add controller CourseManager

---
+++ IsuExtra/Controllers/CourseManager.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using IsuExtra.Interfaces;
+using IsuExtra.Models;
+using IsuExtra.Tools;
+
+namespace IsuExtra.Controllers
+{
+    public class CourseManager : ICourseManager
+    {
+        private List<Course> Courses { get; } = new List<Course>();
+        private List<CourseFlow> CoursesFlows { get; } = new List<CourseFlow>();
+        private readonly StudentManager _studentManager;
+
+        public CourseManager(StudentManager studentManager)
+        {
+            _studentManager = studentManager;
+        }
+
+        public Course CreateCourse(string name)
+        {
+            Course course = new Course(Courses.Count, name);
+            Courses.Add(course);
+            return course;
+        }
+
+        public CourseFlow GetStudents(int courseId)
+        {
+            try
+            {
+                CourseFlow students = CoursesFlows.First(s => s.Course.Id == courseId);
+                return students;
+            }
+            catch (Exception e)
+            {
+                throw new IsuExtraException(e.Message);
+            }
+        }
+
+        public CourseFlow RegisterStudentToCourse(Course course, List<StudentEnrollment> studentsEnrollments)
+        {
+            var nameCourse = Courses.ElementAt(course.Id).Name.Substring(0, 1);
+            const int maxCount = 10;
+            foreach (var st in studentsEnrollments.Select(student => new CourseFlow()
+                {Course = course, Student = student.Student}))
+            {
+                if (nameCourse != st.Student.Group.Name.Substring(0, 1) && CoursesFlows.Count < maxCount)
+                {
+                    CoursesFlows.Add(st);
+                }
+                else
+                {
+                    throw new IsuExtraException(
+                        "Students in course limit or the student cannot enroll in their group's course");
+                }
+            }
+
+            return GetStudents(course.Id);
+        }
+
+        public CourseFlow GetCourseFlow(int courseId)
+        {
+            var result = CoursesFlows.Find(cf => cf.Course.Id == courseId);
+            if (result == null)
+            {
+                throw new IsuExtraException("No such students in course");
+            }
+
+            return result;
+        }
+
+        public int GetCourseFlowCount(int courseId)
+        {
+            var flow = CoursesFlows.Where(c => c.Course.Id == courseId).ToList();
+            return flow.Count;
+        }
+
+        public CourseFlow RemoveStudentOnCourse(int courseId, int studentId)
+        {
+            try
+            {
+                var flow = CoursesFlows.Where(c => c.Course.Id == courseId && c.Student.Id == studentId).ToList();
+                CoursesFlows.RemoveAll(c => c.Student.Id == studentId);
+                return GetStudents(courseId);
+            }
+            catch (Exception e)
+            {
+                throw new IsuExtraException(e.Message);
+            }
+        }
+
+        public List<StudentUnsigned> UnsignedStudentsOnCourse(List<StudentUnsigned> students,
+            List<StudentEnrollment> studentEnrollments, string groupName)
+        {
+            var list = new List<StudentUnsigned>();
+
+            foreach (var res in students.Where(s => s.Student.Group.Name == groupName))
+            {
+                var student = res.Student.Name != null
+                    ? studentEnrollments.FirstOrDefault(s => s.Student.Name == res.Student.Name)
+                    : studentEnrollments.FirstOrDefault(s => s.Student.Id == res.Student.Id);
+                if (student == null)
+                {
+                    list.Add(res);
+                }
+            }
+
+            return list;
+        }
+    }
+}
--- IsuExtra/Program.cs
+++ IsuExtra/Program.cs
@@ -1,9 +1,66 @@
-﻿namespace IsuExtra
+﻿using System;
+using System.Collections.Generic;
+using IsuExtra.Controllers;
+using IsuExtra.Models;
+using static System.Console;
+
+namespace IsuExtra
 {
-    internal class Program
+    class Program
     {
-        private static void Main()
+        static void Main(string[] args)
         {
+            var studentManager = new StudentManager();
+
+            WriteLine("\nAdded groups:\n");
+            Group m1 = studentManager.AddGroup("M1211");
+            Group с2 = studentManager.AddGroup("C2312");
+            WriteLine($"\tId: {m1.Id} \tName: {m1.Name}");
+            WriteLine($"\tId: {с2.Id} \tName: {с2.Name}");
+
+            WriteLine("\nAdded students:\n");
+            Student student1 = studentManager.AddStudent(m1, "rodion");
+            Student student2 = studentManager.AddStudent(m1, "ivan");
+            Student student3 = studentManager.AddStudent(m1, "roman");
+            Student student4 = studentManager.AddStudent(с2, "kirill");
+            Student student5 = studentManager.AddStudent(с2, "daniil");
+            Student student6 = studentManager.AddStudent(с2, "alex");
+            WriteLine($"\tId: {student1.Id} \tName: {student1.Name} \tGroup: {student1.Group.Name}");
+            WriteLine($"\tId: {student2.Id} \tName: {student2.Name} \tGroup: {student2.Group.Name}");
+            WriteLine($"\tId: {student3.Id} \tName: {student3.Name} \tGroup: {student3.Group.Name}");
+
+            var courseManager = new CourseManager(studentManager);
+            WriteLine("\nAdded courses:\n");
+            Course ct = courseManager.CreateCourse("Computer technology");
+            WriteLine($"\tCourseId: {ct.Id}, \tCourseName: {ct.Name}");
+            Course mg = courseManager.CreateCourse("Mathematical technology");
+            WriteLine($"\tCourseId: {mg.Id}, \tCourseName: {mg.Name}");
+
+            WriteLine("\nAdded students to course:\n");
+            var studentEnrollment = new List<StudentEnrollment>
+            {
+                new StudentEnrollment(student2),
+                new StudentEnrollment(student3),
+            };
+            courseManager.RegisterStudentToCourse(ct, studentEnrollment);
+            foreach (var se in studentEnrollment)
+            {
+                WriteLine(
+                    $"\tCourseId: {ct.Id}, \tCourse: {ct.Name}, \tStudentId: {se.Student.Id}, \tStudentName: {se.Student.Name}, \tStudentGroup: {se.Student.Group.Name}");
+            }
+
+            WriteLine("\nAll unsigned students on course:\n");
+
+            var studentUnsigned = new List<StudentUnsigned>
+            {
+                new StudentUnsigned(student1),
+                new StudentUnsigned(student2),
+                new StudentUnsigned(student3),
+                new StudentUnsigned(student4),
+                new StudentUnsigned(student5),
+                new StudentUnsigned(student6),
+            };
+            courseManager.UnsignedStudentsOnCourse(studentUnsigned, studentEnrollment, "M1211");
         }
     }
 }
