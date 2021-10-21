d92561e71ef6782fd5f692d21b001e2104374c2d
lifeonh1gh <lifeonh1gh@mail.ru>
2021-10-21 16:07:05 +0300

    Added functional from Isu

---
+++ IsuExtra/Models/Group.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using IsuExtra.Tools;
+
+namespace IsuExtra.Models
+{
+    public class Group
+    {
+        public Group(int id, string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new IsuExtraException("Unable to create Group", new ArgumentNullException(nameof(name)));
+            }
+
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; }
+        public string Name { get; }
+    }
+}
---
+++ IsuExtra/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using IsuExtra.Tools;
+
+namespace IsuExtra.Models
+{
+    public class Student
+    {
+        public Student(int id, string name, Group group)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new IsuExtraException("Unable to create Student", new ArgumentNullException(nameof(name)));
+            }
+
+            Id = id;
+            Name = name;
+            Group = group ?? throw new NullReferenceException(nameof(Group.Name));
+        }
+
+        public int Id { get; }
+        public string Name { get; set; }
+        public Group Group { get; }
+    }
+}
