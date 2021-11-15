f624601af523cff3725bd9193179b52ef288bdfa
lifeonh1gh <lifeonh1gh@mail.ru>
2021-11-15 15:43:44 +0300

    add repositories

---
+++ Banks/Repositories/AccountsRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Banks.Models.Accounts;
+
+namespace Banks.Repositories
+{
+    public class AccountsRepository
+    {
+        public List<Account> Accounts { get; } = new List<Account>();
+
+        public void AddAccount(Account account)
+        {
+            Accounts.Add(account);
+        }
+
+        public Account GetAccount(int id)
+            => Accounts.FirstOrDefault(a => a.AccountId == id);
+
+        public IEnumerable<Account> GetAccounts()
+            => Accounts;
+    }
+}
---
+++ Banks/Repositories/BanksRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Banks.Models.Banks;
+
+namespace Banks.Repositories
+{
+    public class BanksRepository
+    {
+        public List<Bank> Banks { get; } = new List<Bank>();
+
+        public void AddBank(Bank bank)
+        {
+            Banks.Add(bank);
+        }
+
+        public Bank GetBank(int id) =>
+            Banks.FirstOrDefault(b => b.Id == id);
+
+        public IEnumerable<Bank> GetBanks()
+            => Banks;
+    }
+}
